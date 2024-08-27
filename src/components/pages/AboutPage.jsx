b3730f808e5aacfe2677f6bcf8a0151c0657a5fa
Nouraldeen504 <nouraldeen.albakoush@gmail.com>
2024-08-27 18:46:02 +0300

    added content for contact us and about us pages

--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -1,12 +1,64 @@
-// src/pages/AboutPage.js
 import React from 'react';
+import shop from '../../assets/images/shop.jpeg';
+import { Link } from 'react-router-dom';
 
 const AboutPage = () => {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>This is the About page.</p>
-    </div>
+    <>
+      <div className='container col-xxl-8 px-4 py-5'>
+        <div class="px-4 py-5 my-5 text-center">
+          <h1 class="display-5 fw-bold text-body-emphasis">زويتة</h1>
+            <div class="col-lg-6 mx-auto">
+              <p class="lead mb-4">وجهتك المثالية للأناقة، الراحة، والابتكار</p>
+            </div>
+        </div>
+      </div>
+
+      <hr />
+
+      <div class="container col-xxl-8 px-4 py-5">
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5 mx-auto">
+          <div class="col-10 col-sm-8 col-lg-6">
+            <img src={shop} class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="400" height="300" loading="lazy" />
+          </div>
+          <div class="col-lg-6">
+            <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">من نحن</h1>
+            <p class="lead">في زويتة، نقدم مجموعة مختارة بعناية من الملابس، الأحذية، الأجهزة المنزلية، المكياج، والإكسسوارات. مهمتنا هي تقديم أفضل ما في الموضة والوظائف إلى باب منزلك.</p>
+          </div>
+        </div>
+      </div>
+
+      <hr />
+
+      <div class="container col-xxl-8 px-4 py-5">
+        <div class="row flex-lg align-items-start g-5 py-5 mx-auto">
+          <div class="col-lg-6">
+              <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">قصتنا</h1>
+              <p class="lead">تأسست زويتة في عام [السنة]، وبدأ كمشروع عائلي صغير بشغف لتقديم منتجات عالية الجودة بأسعار معقولة. على مر السنين، نمونا لنصبح سوقًا موثوقًا به ومعروفًا بتنوع منتجاتنا وخدمة العملاء الاستثنائية.</p>
+          </div>
+          <div class="col-lg-6">
+            <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">قيمنا</h1>
+            <p class="lead">نؤمن بالجودة، الاستدامة، ورضا العملاء. يعمل فريقنا بجد لتوفير منتجات ليست فقط جميلة ولكنها تؤدي أداءً متميزًا.</p>
+          </div>
+        </div>
+      </div>
+
+      <hr />
+
+      <div class="container col-xxl-8 px-4 py-5">
+        <div class="row flex-lg align-items-center py-5 text-center">
+          <h1 class="display-5 fw-bold text-body-emphasis">اتصل بنا</h1>
+          <div class="col-lg-6 mx-auto">
+            <p class="lead">نحن نحب أن نسمع منك! تواصل معنا على [البريد الإلكتروني] أو تابعنا على [روابط وسائل التواصل الاجتماعي].</p>
+            <Link to="/contact" style={{textDecoration: 'none'}}>
+              <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                <button type="button" class="btn btn-dark btn-lg px-4 me-sm-3 fw-bold">اعرف المزيد</button>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/components/pages/ContactPage.jsx
+++ src/components/pages/ContactPage.jsx
@@ -3,10 +3,69 @@ import React from 'react';
 
 const ContactPage = () => {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>This is the Contact page.</p>
-    </div>
+    <>
+      <div className='container col-xxl-8 px-4 py-5'>
+        <div class="px-4 py-5 my-5 text-center">
+          <h1 class="display-5 fw-bold text-body-emphasis">اتصل بنا</h1>
+            <div class="col-lg-6 mx-auto">
+              <p class="lead mb-4">نحن هنا للإجابة على جميع استفساراتك ومساعدتك في كل ما تحتاجه</p>
+            </div>
+        </div>
+      </div>
+
+      <hr />
+
+      <div class="container col-xxl-8 px-4 py-5">
+        <div class="row flex-lg align-items-start g-3 py-3">
+          <div class="col-lg-6 mx-auto text-center">
+              <h2 class="fw-bold text-body-emphasis lh-1 mb-3">ساعات العمل</h2>
+              <p class="lead">10:00-19:00</p>
+          </div>
+          <div class="col-lg-6 mx-auto text-center">
+            <h2 class="fw-bold text-body-emphasis lh-1 mb-3">العنوان</h2>
+            <p class="lead">شارع بن عاشور</p>
+          </div>
+        </div>
+        <div class="row flex-lg align-items-start g-3 py-3">
+          <div class="col-lg-6 mx-auto text-center">
+              <h2 class="fw-bold text-body-emphasis lh-1 mb-3">البريد الالكتروني</h2>
+              <p class="lead">info@zweta.ly</p>
+          </div>
+          <div class="col-lg-6 mx-auto text-center">
+            <h2 class="fw-bold text-body-emphasis lh-1 mb-3">ارقامنا</h2>
+            <div className='lh-1' dir="ltr">
+              <p class="lead">00 123456789</p>
+              <p class="lead">00 123456789</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr />
+
+      <div className='container col-xxl-8 px-4 py-5'>
+        <div class="px-4 text-center">
+          <h1 class="fw-bold text-body-emphasis">راسلنا</h1>
+            <form class="">
+              <div className='d-flex flex-column-sm justify-content-center gap-5'>
+                <div class="d-flex flex-column col-4 gap-3">
+                  <input type="text" class="form-control" id="formName" placeholder='الاسم'/>
+                  <input type="text" class="form-control" id="formSubject" placeholder='الموضوع' />
+                  <input type="email" class="form-control" id="formEmail" placeholder='البريد الالكتروني' />
+                </div>
+                <div className='col-4'>
+                  <textarea name="content" id="formContent" className='w-100 h-100'></textarea>
+                </div>
+              </div>
+              <div class="text-end">
+                <button type="submit" class="btn btn-subtle me-2">Cancel</button>
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
+        </div>
+      </div>
+
+    </>
   );
 };
 
